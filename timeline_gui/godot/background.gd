2dc9e5873c5e7c06e34a3933d06c5e43b477735d
Leandro Vandari <leandrovandari@gmail.com>
2026-04-15 11:51:34 +0200

    fmt

--- timeline_gui/godot/background.gd
+++ timeline_gui/godot/background.gd
@@ -6,26 +6,28 @@ const EXTRA_ROOM_FACTOR: float = 1.2
 var offset: float = 0.
 var num_lines: int = 0
 
+
 func _draw() -> void:
 	var YEAR_FONT = ThemeDB.fallback_font
 	print_verbose("Redrawing background lines...")
 	var size = viewport.get_visible_rect().size
-	var line_dist = (size.x*EXTRA_ROOM_FACTOR) / num_lines
+	var line_dist = (size.x * EXTRA_ROOM_FACTOR) / num_lines
 	offset = fposmod(offset, line_dist)
 	var marker_iter = LineMarkerIterator.create("1970-01-01T00:00:00+00:00[+00:00]", 0)
 
 	for i in range(num_lines):
-		var x_pos = (-size.x*EXTRA_ROOM_FACTOR) / 2 + offset + line_dist * i;
-		draw_line(
-			Vector2(x_pos, -size.y / 2),
-			Vector2(x_pos, size.y / 2),
-			Color.DARK_GRAY
-		)
+		var x_pos = (-size.x * EXTRA_ROOM_FACTOR) / 2 + offset + line_dist * i
+		draw_line(Vector2(x_pos, -size.y / 2), Vector2(x_pos, size.y / 2), Color.DARK_GRAY)
 
 		var year = marker_iter.next_marker()
 
 		var year_text_size = YEAR_FONT.get_string_size(year.marker_str)
-		draw_string(YEAR_FONT, Vector2(x_pos - year_text_size.x/2, MainLine.vertical_offset + 20.), year.marker_str, HORIZONTAL_ALIGNMENT_CENTER)
+		draw_string(
+			YEAR_FONT,
+			Vector2(x_pos - year_text_size.x / 2, MainLine.vertical_offset + 20.),
+			year.marker_str,
+			HORIZONTAL_ALIGNMENT_CENTER
+		)
 
 
 func _ready() -> void:
@@ -34,12 +36,12 @@ func _ready() -> void:
 	# initialize the offset so that the starting year isn't exactly on the screen edge
 	var size = viewport.get_visible_rect().size
 	var line_dist = size.x / num_lines
-	offset = line_dist/2
+	offset = line_dist / 2
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_action_pressed("timeline_drag"):
 		offset += event.relative.x
-
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
