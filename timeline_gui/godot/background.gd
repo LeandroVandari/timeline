db4592547597cc9eb7970e15f971d997b5cf1fab
Leandro Vandari <leandrovandari@gmail.com>
2026-04-24 10:31:36 +0200

    fix warnings by declaring all types

--- timeline_gui/godot/background.gd
+++ timeline_gui/godot/background.gd
@@ -8,20 +8,20 @@ var num_lines: int = 0
 
 
 func _draw() -> void:
-	var YEAR_FONT = ThemeDB.fallback_font
+	var YEAR_FONT: Font = ThemeDB.fallback_font
 	print_verbose("Redrawing background lines...")
-	var size = viewport.get_visible_rect().size
-	var line_dist = (size.x * EXTRA_MARKER_ROOM) / num_lines
+	var size: Vector2 = viewport.get_visible_rect().size
+	var line_dist: float = (size.x * EXTRA_MARKER_ROOM) / num_lines
 	offset = fposmod(offset, line_dist)
-	var marker_iter = LineMarkerIterator.create_from_now( 0)
+	var marker_iter: LineMarkerIterator = LineMarkerIterator.create_from_now( 0)
 
-	for i in range(num_lines):
-		var x_pos = (-size.x * EXTRA_MARKER_ROOM) / 2 + offset + line_dist * i
+	for i: int in range(num_lines):
+		var x_pos: float = (-size.x * EXTRA_MARKER_ROOM) / 2 + offset + line_dist * i
 		draw_line(Vector2(x_pos, -size.y / 2), Vector2(x_pos, size.y / 2), Color.DARK_GRAY)
 
-		var year = marker_iter.next_marker()
+		var year: LineMarker = marker_iter.next_marker()
 
-		var year_text_size = YEAR_FONT.get_string_size(year.marker_str)
+		var year_text_size: Vector2 = YEAR_FONT.get_string_size(year.marker_str)
 		draw_string(
 			YEAR_FONT,
 			Vector2(x_pos - year_text_size.x / 2, MainLine.vertical_offset + 20.),
@@ -34,14 +34,15 @@ func _ready() -> void:
 	num_lines = Timeline.years_width()
 
 	# initialize the offset so that the starting year isn't exactly on the screen edge
-	var size = viewport.get_visible_rect().size
-	var line_dist = size.x / num_lines
+	var size: Vector2 = viewport.get_visible_rect().size
+	var line_dist: float = size.x / num_lines
 	offset = line_dist / 2
 
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion and Input.is_action_pressed("timeline_drag"):
-		offset += event.relative.x
+	if  event is InputEventMouseMotion and Input.is_action_pressed("timeline_drag"):
+
+		offset += (event as InputEventMouseMotion).relative.x
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
