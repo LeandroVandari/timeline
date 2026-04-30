283ddf856924092afa1a67cb6a1976af89798559
Leandro Vandari <leandrovandari@gmail.com>
2026-04-30 12:12:04 +0200

    rewrite background lines to be abhangig from a set line distance instead of a number of lines so that i can properly write zoom levels

--- timeline_gui/godot/background.gd
+++ timeline_gui/godot/background.gd
@@ -1,9 +1,8 @@
 extends Node2D
 @onready var viewport: Viewport = get_viewport()
 @onready var timeline: Timeline = $".."
-const EXTRA_MARKER_ROOM: float = 1.1
 
-@export var num_lines: int
+@export var line_dist: float = 100.
 var show_months: bool
 
 var year_start: Year = Year.get_current()
@@ -16,7 +15,7 @@ func _draw() -> void:
 	print_verbose("Redrawing background lines...")
 
 	var size: Vector2 = viewport.get_visible_rect().size
-	var line_dist: float = (size.x * EXTRA_MARKER_ROOM) / num_lines
+	var num_lines: float = ceil(size.x / line_dist) + 2
 
 	# right edge of screen
 	if offset <= 0:
@@ -28,8 +27,10 @@ func _draw() -> void:
 
 	var year_iter: YearIterator = YearIterator.create(year_start)
 	for i: int in range(num_lines):
-		var year_x_pos: float = (-size.x * EXTRA_MARKER_ROOM) / 2 + offset + line_dist * i
-		draw_line(Vector2(year_x_pos, -size.y / 2), Vector2(year_x_pos, size.y / 2), Color.DARK_GRAY)
+		var year_x_pos: float = (-line_dist * num_lines) / 2 + line_dist * i + offset
+		draw_line(
+			Vector2(year_x_pos, -size.y / 2), Vector2(year_x_pos, size.y / 2), Color.DARK_GRAY
+		)
 
 		var year: Year = year_iter.next_year()
 		var year_text: String = year.label()
@@ -43,28 +44,28 @@ func _draw() -> void:
 		)
 
 		if show_months:
-
 			var month_dist: float = line_dist / year.months_amount()
 			# no enumerate function :(
 			var j: int = 0
 			for month: int in year.months():
 				var month_text: String = str(month)
 				var month_text_size: Vector2 = MONTH_FONT.get_string_size(month_text)
-				draw_string(MONTH_FONT, Vector2(year_x_pos + month_dist * j - month_text_size.x/2, MainLine.vertical_offset+15.), month_text, HORIZONTAL_ALIGNMENT_CENTER, -1, 12)
+				draw_string(
+					MONTH_FONT,
+					Vector2(
+						year_x_pos + month_dist * j - month_text_size.x / 2,
+						MainLine.vertical_offset + 15.
+					),
+					month_text,
+					HORIZONTAL_ALIGNMENT_CENTER,
+					-1,
+					12
+				)
 				j += 1
 
 
-
-
-
-
-
-
 func _ready() -> void:
-
 	# initialize the offset so that the starting year isn't exactly on the screen edge
-	var size: Vector2 = viewport.get_visible_rect().size
-	var line_dist: float = size.x / num_lines
 	offset = line_dist / 2
 
 
