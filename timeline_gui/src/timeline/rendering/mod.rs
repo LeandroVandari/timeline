4c9efde41e5668bee092cff931f19e105c53672e
Leandro Vandari <leandrovandari@gmail.com>
2026-05-08 10:48:36 +0200

    Add basics for timeline drag, for now just detection and invisible background

--- timeline_gui/src/timeline/rendering/mod.rs
+++ timeline_gui/src/timeline/rendering/mod.rs
@@ -16,7 +16,11 @@ impl Plugin for TimelineRendererPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (Self::spawn_timeline_camera, Self::spawn_timeline_lines)
+            (
+                Self::spawn_timeline_camera,
+                Self::spawn_timeline_lines,
+                Self::spawn_dragging_background,
+            )
                 .run_if(on_message::<TimelineRenderInformationCreatedMessage>),
         )
         .add_observer(
@@ -155,6 +159,44 @@ impl TimelineRendererPlugin {
                     )],
                 ));
             }
+        }
+    }
+
+    #[instrument(skip_all)]
+    fn spawn_dragging_background(
+        mut commands: Commands,
+        render_info_query: Query<(&TimelineRenderInformation, &Transform)>,
+        mut added_render_infos: MessageReader<TimelineRenderInformationCreatedMessage>,
+        window: Single<&Window, With<PrimaryWindow>>,
+    ) {
+        for msg in added_render_infos.read() {
+            let entity = msg.entity();
+            trace!("Spawning dragging background for timeline {entity}");
+            let (render_info, pos) = render_info_query
+                .get(entity)
+                .expect("Message should refer to an entity with proper components");
+            let timeline_size = render_info.size.unwrap_or(window.size());
+
+            let background_entity = commands
+                .spawn((
+                    Sprite {
+                        custom_size: Some(timeline_size),
+                        color: if cfg!(feature="debug") {Color::srgba(0.5, 0.,0., 0.5)} else {Color::NONE},
+                        ..Default::default()
+
+                    },
+                    pos.with_translation(Vec3::new(0.,0.,-100.)),
+                    Pickable::default(),
+                    Self::get_render_layer(msg.entity())
+                ))
+                .observe(|trigger: On<Pointer<Drag>>| {
+                    if matches!(trigger.button, PointerButton::Primary) {
+                        info!("Dragging timeline");
+                    }
+                })
+                .id();
+
+            commands.entity(entity).add_child(background_entity);
         }
     }
 
