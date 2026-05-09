5c6ba12548d9ff0aac3855960c8493b8422e840e
Leandro Vandari <leandrovandari@gmail.com>
2026-05-09 22:57:33 +0200

    Update crates and make app vsync update so it's not weird

--- timeline_gui/src/main.rs
+++ timeline_gui/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, winit::WinitSettings};
 
 use crate::{setup::SetupPlugin, timeline::rendering::TimelineRendererPlugin};
 
@@ -10,11 +10,12 @@ fn main() -> AppExit {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "Timeline Creator".into(),
-                present_mode: bevy::window::PresentMode::AutoNoVsync,
+                present_mode: bevy::window::PresentMode::Fifo,
                 ..Default::default()
             }),
             ..Default::default()
         }))
+        .insert_resource(WinitSettings::desktop_app())
         .insert_resource(ClearColor(Color::hsv(0., 0., 0.3)))
         .add_plugins((SetupPlugin, TimelineRendererPlugin))
         .run()
