5ad8d28918887a6bad7dfd5347ab38b7823c8794
Leandro Vandari <leandrovandari@gmail.com>
2026-06-27 19:53:28 +0200

    rewrite vertical lines spawning to spawn batches, which is more performant

--- timeline_gui/src/timeline/rendering/lines/mod.rs
+++ timeline_gui/src/timeline/rendering/lines/mod.rs
@@ -26,7 +26,8 @@ impl Plugin for TimelineLinesPlugin {
             )
                 .chain()
                 .run_if(on_message::<RenderedTimelineCreatedMessage>),
-        );
+        )
+        .add_observer(Self::year_label_wrap_around);
     }
 }
 
@@ -37,51 +38,74 @@ impl TimelineLinesPlugin {
         timeline_entity: Entity,
 
         timeline_info: Query<(&Transform, &VerticalLineRenderInfo, &RenderLayers)>,
-        lines: impl Iterator<Item = (f32, Year)>,
+        lines: impl Iterator<Item = (f32, Year)> + Send + Sync + 'static + Clone,
     ) -> Result<(), QueryEntityError> {
         trace!("Spawning vertical lines for timeline {timeline_entity}");
         let (pos, render_info, render_layers) = timeline_info.get(timeline_entity)?;
 
-        commands.entity(timeline_entity).with_children(|spawner|{
-            for (line_x_pos, year) in lines {
-            spawner.spawn((
+        {
+            let pos = *pos;
+            let render_info = render_info.to_owned();
+            let render_layers = render_layers.to_owned();
+            let lines = lines.clone().map(move |(line_x_pos, _year)| {
+                (
                     render_info.wrap_info,
                     HorizontallyDraggedBy(timeline_entity),
                     Mesh2d(render_info.mesh.clone()),
                     MeshMaterial2d(render_info.material.clone()),
                     pos.with_translation(Vec3::new(line_x_pos, 0., 0.)),
                     render_layers.clone(),
-                ));
-                spawner
-                    .spawn((
-                        render_info.wrap_info,
-                        DraggedBy::new(timeline_entity),
-                        Text2d::new(year.to_string()),
-                        YearLabel(year),
-                        pos.with_translation(Vec3::new(line_x_pos, -15., 0.)),
-                        render_layers.clone(),
-                    ))
-                    .observe(
-                        move |trigger: On<WrapAround>,
-                              mut label_query: Query<(&mut YearLabel, &mut Text2d)>,
-                              mut range_query: Query<&mut TimelineRenderRange>| {
-                            let (mut year, mut text_label) =
-                                label_query.get_mut(trigger.entity).expect("The entity has a Text2d and YearLabel");
-                            let mut range = range_query.get_mut(timeline_entity).expect("`entity` is the RenderedTimeline which also has a TimelineRenderRange.");
-                            match trigger.direction {
-                                WrapDirection::Left => {
-                                    range.inc();
-                                    year.0 = range.0.end.clone();},
-                                WrapDirection::Right => {
-                                    range.dec();
-                                    year.0 = range.0.start.clone();
-                                }
-                            }
-                            text_label.0 = year.0.to_string();
-                        },
-        );}});
+                    ChildOf(timeline_entity),
+                )
+            });
+
+            commands.spawn_batch(lines);
+        }
+
+        {
+            let pos = *pos;
+            let render_info = render_info.to_owned();
+            let render_layers = render_layers.to_owned();
+
+            let labels = lines.map(move |(line_x_pos, year)| {
+                (
+                    render_info.wrap_info,
+                    DraggedBy::new(timeline_entity),
+                    Text2d::new(year.to_string()),
+                    YearLabel(year),
+                    pos.with_translation(Vec3::new(line_x_pos, -15., 0.)),
+                    render_layers.clone(),
+                    ChildOf(timeline_entity),
+                )
+            });
+            commands.spawn_batch(labels);
+        }
 
         Ok(())
+    }
+
+    fn year_label_wrap_around(
+        trigger: On<WrapAround>,
+        mut label_query: Query<(&mut YearLabel, &mut Text2d, &ChildOf)>,
+        mut range_query: Query<&mut TimelineRenderRange>,
+    ) {
+        let Ok((mut year, mut text_label, parent)) = label_query.get_mut(trigger.entity) else {
+            return;
+        };
+        let mut range = range_query
+            .get_mut(parent.0)
+            .expect("`entity` is the RenderedTimeline which also has a TimelineRenderRange.");
+        match trigger.direction {
+            WrapDirection::Left => {
+                range.inc();
+                year.0 = range.0.end.clone();
+            }
+            WrapDirection::Right => {
+                range.dec();
+                year.0 = range.0.start.clone();
+            }
+        }
+        text_label.0 = year.0.to_string();
     }
 }
 
