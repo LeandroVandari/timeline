c15f64c495cdfea3bf857296d3536809151ec122
Leandro Vandari <leandrovandari@gmail.com>
2026-07-27 21:04:01 +0200

    first_test

---
+++ timeline_bg.wasm.d.ts
@@ -0,0 +1,23 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wasm_bindgen_func_elem_79865: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_79926: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_88134: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_88134_3: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_88134_4: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_88134_5: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_88134_6: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_88134_7: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_88134_8: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_88134_9: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_90040: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_90145: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_export5: (a: number, b: number) => void;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
+export const __wbindgen_start: () => void;
