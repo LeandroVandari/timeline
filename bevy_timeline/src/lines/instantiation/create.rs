1a45b576946790bc898a6c674d146cd55c9113ac
Leandro Vandari <leandrovandari@gmail.com>
2026-08-05 11:35:29 +0200

    Split deficit calculation and line creation

---
+++ bevy_timeline/src/lines/instantiation/create.rs
@@ -0,0 +1,101 @@
+use bevy::{camera::visibility::RenderLayers, ecs::query::QueryEntityError, prelude::*};
+use bevy_zoom::ZoomLevel;
+use timeline_core::date_iteration::YearIterator;
+
+use super::line_deficit::LineDeficit;
+use crate::{
+    RenderedTimeline,
+    configuration::{
+        TimelineHorizontalOffset, TimelineLineSeparation, TimelineRenderRange,
+        TimelineVerticalOffset,
+    },
+    lines::VerticalLineRenderInfo,
+};
+
+#[expect(clippy::cast_precision_loss, reason = "Lines layouting is best effort")]
+pub fn create_lines(
+    mut deficit_reader: PopulatedMessageReader<LineDeficit>,
+    mut timeline_info: Query<(
+        &ZoomLevel,
+        &TimelineLineSeparation,
+        &mut TimelineRenderRange,
+        &mut TimelineHorizontalOffset,
+    )>,
+
+    spawn_lines_info: Query<(
+        &Transform,
+        &VerticalLineRenderInfo,
+        &RenderLayers,
+        &TimelineVerticalOffset,
+        &ZoomLevel,
+    )>,
+    mut commands: Commands,
+) {
+    for deficit in deficit_reader.read() {
+        let (&zoom_level, &line_separation, mut render_range, mut offset) = match timeline_info
+            .get_mut(deficit.timeline)
+        {
+            Ok(tup) => tup,
+            Err(QueryEntityError::QueryDoesNotMatch(_, _)) => {
+                warn!(
+                    "Timeline didn't contain required components for handling zoom of vertical lines"
+                );
+                continue;
+            }
+            Err(e) => {
+                error!("Error matching query while handling zoom for timeline lines: {e}");
+                continue;
+            }
+        };
+
+        let scaled_line_separation = *line_separation * *zoom_level;
+        let occupied_space =
+            RenderedTimeline::draw_width(&render_range, line_separation, zoom_level);
+
+        if deficit.right > 0 {
+            let right_iter = YearIterator::new(&render_range.0.end)
+                .skip(1)
+                .take(deficit.right.cast_unsigned());
+            render_range.0.end = right_iter.clone().last().unwrap();
+            let offset = **offset;
+            crate::lines::TimelineLinesPlugin::spawn_vertical_lines(
+                &mut commands,
+                deficit.timeline,
+                spawn_lines_info,
+                right_iter.enumerate().map(move |(i, y)| {
+                    (
+                        scaled_line_separation
+                            .mul_add((i + 1) as f32, occupied_space / 2. + offset),
+                        y,
+                    )
+                }),
+            )
+            .unwrap();
+        }
+
+        if deficit.left > 0 {
+            let left_iter = YearIterator::new(&render_range.0.start)
+                .rev()
+                .skip(1)
+                .take(deficit.left.cast_unsigned());
+            render_range.0.start = left_iter.clone().last().unwrap();
+            let offset = **offset;
+            crate::lines::TimelineLinesPlugin::spawn_vertical_lines(
+                &mut commands,
+                deficit.timeline,
+                spawn_lines_info,
+                left_iter.enumerate().map(move |(i, y)| {
+                    (
+                        (-scaled_line_separation)
+                            .mul_add((i + 1) as f32, -occupied_space / 2. + offset),
+                        y,
+                    )
+                }),
+            )
+            .unwrap();
+        }
+
+        **offset +=
+            (deficit.right.max(0) - deficit.left.max(0)) as f32 * scaled_line_separation / 2.;
+    }
+}
